00780ec98035c453a5d97a55d2204e7c4edc04dd
dreamswiki <47926895+dreamswiki@users.noreply.github.com>
2020-09-13 14:47:29 -0400

    Reduced to just web client

--- lib/utils/database/unsupported.dart
+++
@@ -1,9 +0,0 @@
-import 'package:famedlysdk/famedlysdk.dart';
-
-Database constructDb({bool logStatements = false, String filename = 'database.sqlite', String password = ''}) {
-  throw 'Platform not supported';
-}
-
-Future<String> getLocalstorage(String key) async {
-  return null;
-}
